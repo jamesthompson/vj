d882a68f1d39d9a4f5854eb70d9fa3802ae121c0
James Thompson <jamesrth@usc.edu>
2013-11-19 13:39:38 -0800

    'refactored a little'

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,14 +7,21 @@ import org.jsoup.select.Elements
 import scala.collection.JavaConverters._
 import java.io.PrintWriter
 
-case class History(filed: Option[DateTime], noa: Option[DateTime], bio: Option[DateTime], appr: Option[DateTime], rec: Option[DateTime]) {
+case class History(filed: Option[DateTime],
+                   noa: Option[DateTime], 
+                   bio: Option[DateTime], 
+                   appr: Option[DateTime], 
+                   rec: Option[DateTime]) {
+
   def timeDiff(start: Option[DateTime], finish: Option[DateTime]): Option[Long] = (start, finish) match {
     case (Some(d1), Some(d2)) => if(d1 < d2) Some((d1 to d2).toDuration.getStandardDays) else None
     case (_, _) => None
   }
+
   def filedToAppr = timeDiff(filed, appr)
   def bioToAppr = timeDiff(bio, appr)
   def apprToRec = timeDiff(appr, rec)
+
 }
 
 object Main extends App {
@@ -22,14 +29,17 @@ object Main extends App {
   implicit def mkdate(s: String): Option[DateTime] = try{ Some(new DateTime(s)) } catch { case _ => None }
   
   def getPageHistories(pageNum: Int): List[History] = {
+
     def getHistory(l: List[Element]): Option[History] = l match {
       case f :: n :: b :: a :: r => Some(History(f.html, n.html, b.html, a.html, r.head.html))
       case _ => None
     }
+
     val urlString = s"http://www.visajourney.com/timeline/eadlist.php?cfl=0&op1=&op2=&op3=&op4=$pageNum&op5=4,7,11,12&op6="
     val doc = Jsoup.connect(urlString).get
     val table = doc.select("table[class*=pme-main]")
     val trs = table.select("tr").asScala.toList.tail
+
     trs.map(t => getHistory(t.select("td").asScala.toList.drop(3))).flatMap(h => h)
   }
 
