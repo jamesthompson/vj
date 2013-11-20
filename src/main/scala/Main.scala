9e4261503512af2340ecac4b3abc750e9ef1e326
James Thompson <jamesrth@usc.edu>
2013-11-20 12:50:43 -0800

    refactored for brevity

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,11 @@
 package vj
 
 import com.github.nscala_time.time.Imports._
+import java.io.PrintWriter
 import org.jsoup.Jsoup
 import org.jsoup.nodes.{Document, Element}
 import org.jsoup.select.Elements
 import scala.collection.JavaConverters._
-import java.io.PrintWriter
 
 case class History(filed: Option[DateTime],
                    noa: Option[DateTime], 
@@ -13,55 +13,44 @@ case class History(filed: Option[DateTime],
                    appr: Option[DateTime], 
                    rec: Option[DateTime]) {
 
-  def timeDiff(start: Option[DateTime], finish: Option[DateTime]): Option[Long] = (start, finish) match {
-    case (Some(d1), Some(d2)) => if(d1 < d2) Some((d1 to d2).toDuration.getStandardDays) else None
-    case (_, _) => None
-  }
+  def timeDiff(start: Option[DateTime], finish: Option[DateTime]): Option[Long] = 
+    (start, finish) match {
+      case (Some(d1), Some(d2)) => if(d1 < d2) Some((d1 to d2).toDuration.getStandardDays) else None
+      case (_, _) => None 
+    }
 
   def filedToAppr = timeDiff(filed, appr)
   def bioToAppr = timeDiff(bio, appr)
   def apprToRec = timeDiff(appr, rec)
   def noaToAppr = timeDiff(noa, appr)
-
 }
 
 object Main extends App {
 
   implicit def mkdate(s: String): Option[DateTime] = 
-    try{ Some(new DateTime(s)) } catch { case _ => None }
+    try{ Some(new DateTime(s)) } catch { case _: Throwable => None }
+
+  def printToFile(c: String, l: String) =
+    Some(new java.io.PrintWriter(s"./$l")).foreach{f => try{f.write(c)} finally{f.close}}
   
   def getPageHistories(pageNum: Int): List[History] = {
-
     def getHistory(l: List[Element]): Option[History] = l match {
       case f :: n :: b :: a :: r => Some(History(f.html, n.html, b.html, a.html, r.head.html))
       case _ => None
     }
-
-    val urlString = 
-      s"http://www.visajourney.com/timeline/eadlist.php?cfl=0&op1=&op2=&op3=&op4=$pageNum&op5=4,7,11,12&op6="
-    
-    val doc = Jsoup.connect(urlString).get
-    val table = doc.select("table[class*=pme-main]")
-    val trs = table.select("tr").asScala.toList.tail
-
-    trs.map(t => getHistory(t.select("td").asScala.toList.drop(3))).flatMap(h => h)
+    Jsoup.connect(s"http://www.visajourney.com/timeline/eadlist.php?cfl=0&op1=&op2=&op3=&op4=$pageNum&op5=4,7,11,12&op6=")
+      .get
+      .select("table[class*=pme-main]")
+      .select("tr").asScala.toList.tail.map(
+        t => getHistory(t.select("td").asScala.toList.drop(3))
+      ).flatMap(h => h)
   }
 
-  val histories = (1 to 100).flatMap(getPageHistories)
-
-  val ftoa = histories.map(_.filedToAppr).flatMap(f => f).sorted.mkString("\n")
-  val btoa = histories.map(_.bioToAppr).flatMap(f => f).sorted.mkString("\n")
-  val ator = histories.map(_.apprToRec).flatMap(f => f).sorted.mkString("\n")
-  val ntoa = histories.map(_.noaToAppr).flatMap(f => f).sorted.mkString("\n")
-
-  printToFile(ftoa, "filedToApproval.txt")
-  printToFile(btoa, "biometricsToApproval.txt")
-  printToFile(ator, "approvalToCard.txt")
-  printToFile(ntoa, "noaToApproval.txt")
-
+  val histories = (1 to 50).flatMap(getPageHistories)
+  printToFile(histories.map(_.filedToAppr).flatMap(f => f).mkString("\n"), "filedToApproval.txt")
+  printToFile(histories.map(_.bioToAppr).flatMap(f => f).mkString("\n"), "biometricsToApproval.txt")
+  printToFile(histories.map(_.apprToRec).flatMap(f => f).mkString("\n"), "approvalToCard.txt")
+  printToFile(histories.map(_.noaToAppr).flatMap(f => f).mkString("\n"), "noaToApproval.txt")
   println(s"Data taken from present day to ${histories.last.filed.getOrElse("invalid last day").toString}")
-
-  def printToFile(c: String, l: String) =
-    Some(new java.io.PrintWriter(s"./$l")).foreach{f => try{f.write(c)} finally{f.close}}
 
 }
